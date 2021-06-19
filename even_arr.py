9af89ea05a1976485d8551d11d74684648535f6a
Siddharth Desai <75911033+Sidheartt@users.noreply.github.com>
2021-06-20 00:22:05 +0530

    initial commit
    array with even numbers using numpy

---
+++ even_arr.py
@@ -0,0 +1,19 @@
+# Create an array of all the even integers from 10 to 50
+
+import numpy as np
+
+np.arange(10,51,2)
+
+# identity matrix
+
+np.eye(3)
+
+array([[1., 0., 0.],
+       [0., 1., 0.],
+       [0., 0., 1.]])
+       
+# Use NumPy to generate a random number between 0 and 1
+
+np.random.rand(1)
+
+array([0.55887576])
